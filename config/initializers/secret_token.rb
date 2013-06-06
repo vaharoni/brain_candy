8aa0c8f665240ccedd9a3147a16cfb8c998bb50c
Amit Aharoni <aaharoni@cruisecritic.com>
2013-06-06 10:59:08 -0400

    Bare Rails repo with bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrainCandy::Application.config.secret_token = '57091d0cbfe9ae58c7260000da65cfdc8f04255d865bb0c09451fa5c4daa0984029ad2f306b8fa0fa1978c17a5dbcf0b95b60604fa3f749a50cb2e25cf29d0cd'
